889d1441792bec5067edd39128a3cd1fe67d13ff
github username <github email address>
2021-01-24 13:46:38 -0500

    we got somewhere

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,16 @@
 class Client < ActiveRecord::Base
     has_many :rentals
     has_many :vhs, through: :rentals
+
+    def first_rental(client, movie_title)
+        new_client = Client.create(client)
+        Rental.all.select{|rental| rental.client == self}
+        
+        movie_title = 
+
+    end
+
+
+    
 end
+
