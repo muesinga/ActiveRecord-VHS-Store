2eb114cecb3e36843278e003ecc1069272e19a8c
ajperlis07 <ajperlis07@gmail.com>
2021-01-24 15:00:05 -0500

    update at 3pm

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,15 +2,21 @@ class Client < ActiveRecord::Base
     has_many :rentals
     has_many :vhs, through: :rentals
 
-    def first_rental(client, movie_title)
-        new_client = Client.create(client)
-        Rental.all.select{|rental| rental.client == self}
+    # def first_rental(client, movie_title)
+    #     new_client = Client.create(client)
+    #     Rental.all.select{|rental| rental.client == self}
         
-        movie_title = 
+    #     #movie_title = 
+    # end
 
+    def return_one(vhs)
+        rental = self.rentals.find{|r| r.vhs == vhs}
+        rental.update(current: false)
     end
 
+    def return_all
+        self.rentals.update_all(current: false)
+    end
 
-    
 end
 
